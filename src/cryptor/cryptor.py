df3c35be00f063b135d37bbf2a414f358fc10e23
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-08-23 18:51:51 +0800

    WIP

--- src/cryptor/cryptor.py
+++ src/cryptor/cryptor.py
@@ -1,15 +1,17 @@
 import os
 import msgpack
+import asyncio
+from typing import Union
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
-class HarukiCloudCryptor(object):
+class HarukiCryptor:
     def __init__(self, key):
         self._key = key
 
-    async def pack(self, content):
+    def _pack_sync(self, content: Union[dict, list]) -> bytes:
         nonce = os.urandom(12)
         cipher = Cipher(algorithms.AES(self._key), modes.GCM(nonce), backend=default_backend())
         encryptor = cipher.encryptor()
@@ -18,7 +20,7 @@ class HarukiCloudCryptor(object):
         result = nonce + encryptor.tag + ciphertext
         return result
 
-    async def unpack(self, content):
+    def _unpack_sync(self, content: bytes) -> Union[dict, list]:
         nonce = content[:12]
         tag = content[12:28]
         ciphertext = content[28:]
@@ -26,3 +28,9 @@ class HarukiCloudCryptor(object):
         decryptor = cipher.decryptor()
         decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
         return msgpack.unpackb(decrypted_data, strict_map_key=False)
+
+    async def pack(self, content: Union[dict, list]) -> bytes:
+        return await asyncio.to_thread(self._pack_sync, content)
+
+    async def unpack(self, content: bytes) -> Union[dict, list]:
+        return await asyncio.to_thread(self._unpack_sync, content)
